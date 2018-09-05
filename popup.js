8619ec5e598e8a175b7b7f419df4733280128258
Nezard_lx <411309271@qq.com>
2018-09-05 23:46:44 +0800

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-function geturl()
-{
-	var geturl = document.getElementById("repeat_url");
-	var result = document.getElementById("gbf_url");
-	result.innerHTML = "http://game.granbluefantasy.jp/"+geturl.value;
-	geturl.value = "";
-}
